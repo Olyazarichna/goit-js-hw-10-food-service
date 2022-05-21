64a50eeb8dc9d6120a63c84ad86ec0110ce31376
Olyazarichna <olya.zarichna@gmail.com>
2022-05-21 20:30:36 +0300

    практика

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-import './sass/main.scss';
+import menu from './menu.json';
+import template from './templates/menuItem.hbs';
+
+const refs = {
+  listEl: document.querySelector('.js-menu'),
+  checkBoxEl: document.querySelector('.theme-switch__icon'),
+}
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+}
+
+function createMarkup(menu) {
+    // const markup = menu.map(template);
+    const markup = template(menu);
+    refs.listEl.insertAdjacentHTML('beforend', markup);
+}
+createMarkup(menu);
+
+refs.checkBox.addEventListener('change', checkedBoxChange);
+
+function checkedBoxChange(){
+
+    if(!refs.checkBox.checked) {
+      document.body.classList.add(Theme.LIGHT);
+      document.body.classList.remove(Theme.DARK);
+    } else {
+        document.body.classList.remove(Theme.LIGHT);
+        document.body.classList.add(Theme.DARK);
+    } 
+    // localStorage.setItem();
+}
