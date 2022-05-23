80daadb722a829c4bcb898ac6da8a43c421540e6
Olyazarichna <olya.zarichna@gmail.com>
2022-05-23 15:03:53 +0300

    зміна теми по чекбоксу

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import template from './templates/menuItem.hbs';
 
 const refs = {
   listEl: document.querySelector('.js-menu'),
-  checkBoxEl: document.querySelector('.theme-switch__icon'),
+  checkBoxEl: document.querySelector('.theme-switch__toggle'),
 }
 
 const Theme = {
@@ -12,7 +12,7 @@ const Theme = {
 };
 
 function createMarkup(menu) {
-    // const markup = menu.map(template);
+    // const markup = menu.map(template).join('');
     const markup = template(menu);
     refs.listEl.insertAdjacentHTML('afterbegin', markup);
 }
@@ -21,6 +21,7 @@ createMarkup(menu);
 refs.checkBoxEl.addEventListener('change', checkedBoxChange);
 
 function checkedBoxChange(){
+  console.log('checked');
 
     if(!refs.checkBoxEl.checked) {
       document.body.classList.add(Theme.LIGHT);
@@ -29,5 +30,5 @@ function checkedBoxChange(){
         document.body.classList.remove(Theme.LIGHT);
         document.body.classList.add(Theme.DARK);
     } 
-    // localStorage.setItem();
+    localStorage.setItem();
 }
